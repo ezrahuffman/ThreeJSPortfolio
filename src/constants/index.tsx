ac7aa556473de6a28e610e01076a0ab655bc0b4d
ezrahuffman <35412394+ezrahuffman@users.noreply.github.com>
2025-01-26 17:27:05 -0800

    fill out gladiator project

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -45,29 +45,24 @@ export const navLinks = [
         },
         {
           id: 2,
-          name: 'TailwindCSS',
-          path: 'assets/tailwindcss.png',
+          name: 'Python',
+          path: 'assets/PythonLogo.png',
         },
         {
           id: 3,
           name: 'TypeScript',
           path: '/assets/typescript.png',
         },
-        {
-          id: 4,
-          name: 'Framer Motion',
-          path: '/assets/framer.png',
-        },
       ],
     },
     {
-      title: 'LiveDoc - Real-Time Google Docs Clone',
-      desc: 'LiveDoc is a powerful collaborative app that elevates the capabilities of real-time document editing. As an enhanced version of Google Docs, It supports millions of collaborators simultaneously, ensuring that every change is captured instantly and accurately.',
+      title: 'Gladiator',
+      desc: 'The Gladiator project is my attempt to make intelligent AI in a combat game using Unity AI agents.',
       subdesc:
-        'With LiveDoc, users can experience the future of collaboration, where multiple contributors work together in real time without any lag, by using Next.js and Liveblocks newest features.',
-      href: 'https://www.youtube.com/watch?v=y5vE8y_f_OM',
+        'Gladiator uses Unity AI agents to create intelligent enemies in a third-person video game using unsupervised training, self-play, and the torch library.',
+      href: 'https://www.github.com/ezrahuffman/Gladiator',
       texture: '/textures/project/project2.mp4',
-      logo: '/assets/project-logo2.png',
+      logo: '/assets/crossed_swords.png',
       logoStyle: {
         backgroundColor: '#13202F',
         border: '0.2px solid #17293E',
@@ -77,23 +72,13 @@ export const navLinks = [
       tags: [
         {
           id: 1,
-          name: 'React.js',
-          path: '/assets/react.svg',
+          name: 'CSharp',
+          path: '/assets/C_Sharp_Logo.svg',
         },
         {
           id: 2,
-          name: 'TailwindCSS',
-          path: 'assets/tailwindcss.png',
-        },
-        {
-          id: 3,
-          name: 'TypeScript',
-          path: '/assets/typescript.png',
-        },
-        {
-          id: 4,
-          name: 'Framer Motion',
-          path: '/assets/framer.png',
+          name: 'Unity',
+          path: 'assets/unity.svg',
         },
       ],
     },
